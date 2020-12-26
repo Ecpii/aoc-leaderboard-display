50d340a7a634386cb022b082e00b0b5d75e765ca
Wayne He <epicwaynehe@gmail.com>
2020-12-26 08:28:39 -0500

    data is on screen woo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useMemo } from 'react'
 import './App.css';
 
+import Table from './Table.js'
+
 function App() {
+  const [leaderboardData, setLeaderboardData] = useState([])
+  const fetchLeaderboardData = () => {
+      fetch('data.json',{
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+          }
+      })
+          .then((response) => {
+              console.log(response);
+              return response.json();
+          })
+          .then((data) => {
+              console.log(data);
+              setLeaderboardData(Object.values(data.members));
+          });
+  }
+  const generateTable = () => {
+      let i;
+      let tableColumns = []
+      for (i = 1; i <= 25; i++) {
+        tableColumns.push({
+            Header: 'Day ' + i,
+            columns: [
+                {
+                    Header: 'Part 1',
+                    accessor: 'completion_day_level.' + i + '.1.get_star_ts'
+                },
+                {
+                    Header: 'Part 2',
+                    accessor: 'completion_day_level.' + i + '.2.get_star_ts'
+                }
+            ]
+        })
+      }
+      return tableColumns
+  }
+  const columns = useMemo(
+      generateTable, []
+  )
+
+  useEffect(() => {
+      fetchLeaderboardData()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Advent of Code Leaderboard</h1>
+          <Table columns={columns} data={leaderboardData} />
       </header>
     </div>
   );
---
+++ src/UnixTimeCell.js
@@ -0,0 +1,9 @@
+
+
+export default UnixTimeCell(timestamp) {
+    return (
+        <>
+
+        </>
+    )
+}
