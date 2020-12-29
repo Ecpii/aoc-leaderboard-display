5928a83b36537327126610421eb6b791399cc779
Wayne He <epicwaynehe@gmail.com>
2020-12-29 13:02:32 -0500

    timestamps put in table

--- src/App.js
+++ src/App.js
@@ -21,20 +21,40 @@ function App() {
               setLeaderboardData(Object.values(data.members));
           });
   }
-  const generateTable = () => {
-      let i;
-      let tableColumns = []
-      for (i = 1; i <= 25; i++) {
-        tableColumns.push({
-            Header: 'Day ' + i,
-            columns: [
-                {
+  const generateTimestampCell = ({ cell: {value} }) => {
+        let date = new Date(parseInt(value, 10) * 1000)
+        let day = date.getDate()
+        let hours = '0' + date.getHours()
+        let minutes = '0' + date.getMinutes()
+        let seconds = '0' + date.getSeconds()
+        return (
+            <>
+                <p className='timestamp-day'>
+                    12/{day}
+                </p>
+                <p className='timestamp-cell'>
+                    {hours.substr(-2)}:{minutes.substr(-2)}:{seconds.substr(-2)}
+                </p>
+            </>
+        )
+
+    }
+    const generateTable = () => {
+        let i;
+        let tableColumns = []
+        for (i = 1; i <= 25; i++) {
+            tableColumns.push({
+                Header: 'Day ' + i,
+                columns: [
+                    {
                     Header: 'Part 1',
-                    accessor: 'completion_day_level.' + i + '.1.get_star_ts'
+                    accessor: 'completion_day_level.' + i + '.1.get_star_ts',
+                    Cell: generateTimestampCell
                 },
                 {
                     Header: 'Part 2',
-                    accessor: 'completion_day_level.' + i + '.2.get_star_ts'
+                    accessor: 'completion_day_level.' + i + '.2.get_star_ts',
+                    Cell: generateTimestampCell
                 }
             ]
         })
